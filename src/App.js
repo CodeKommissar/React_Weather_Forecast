6088884dc76acc4160336a88b032f1712752cc05
luisrodriguez <codekommissar@gmail.com>
2018-01-29 21:21:50 -0400

    Add HTTP request with axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  componentDidMount() {
+    axios.get("http://api.openweathermap.org/data/2.5/forecast?id=3646738&APPID=d9db55cd3cf00ab5c19d86aeb4d484c9")
+      .then(response => {
+        console.log(response);
+      })
+  }
+
   render() {
     return (
       <div className="App">
